bad8a123b9a126c272e0502f7eb6553d0d2c2f93
ooosers <yingzi0310@gmail.com>
2015-09-20 21:20:17 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,10 @@
-<?php 
-$link = mysqli_connect('oooserscom.ipagemysql.com', 'oooserscom', '123456'); 
-if (!$link) { 
+<?php	
+	$link = mysqli_connect("79.170.44.213", "web213-ooosers", "HM4t6j!FF", "web213-ooosers");
+
+if (!$link) {
     die('Could not connect: ' . mysqli_error()); 
 } 
-echo 'Connected successfully'; 
-mysqli_select_db('ooosers'); 
-?> 
+
+
+
+?>
